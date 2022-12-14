fe4ffb3340c2a5e5c5d75e207bb7223f60cddae0
Ekaterina Kosinova <ekaterina.kosinova@oxagile.com>
2022-12-14 21:32:02 +0300

    feat: create components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Typography, AppBar } from '@material-ui/core'
 
+import VideoPlayer from './components/VideoPlayer'
+import Options from './components/Options'
+import Notifications from './components/Notifications'
+
 const App = () => {
     return (
         <div>
             <AppBar position='static' color='inherit'>
                 <Typography variant='h2' align='center'>Video Chat App</Typography>
             </AppBar>
+            <VideoPlayer />
+            <Options>
+                <Notifications />
+            </Options>
         </div>
     );
 };
