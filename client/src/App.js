38cf8ca10bd4a6589560a608f2b45fac1b4c33be
Ekaterina Kosinova <ekaterina.kosinova@oxagile.com>
2022-12-15 18:35:29 +0300

    feat: add styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Typography, AppBar } from '@material-ui/core'
-
+import { Typography, AppBar } from '@mui/material'
+import { makeStyles } from '@mui/styles';
 import VideoPlayer from './components/VideoPlayer'
 import Options from './components/Options'
 import Notifications from './components/Notifications'
 
+
 const App = () => {
+
     return (
-        <div>
-            <AppBar position='static' color='inherit'>
+        <div className='wrapper'>
+            <AppBar className='appBar' position='static' color='inherit'>
                 <Typography variant='h2' align='center'>Video Chat App</Typography>
             </AppBar>
             <VideoPlayer />
