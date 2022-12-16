bbc0158d63260149e64e41aec7c032521b0f2880
ekater1na <ekaterina_l@tut.by>
2022-12-16 11:31:11 +0300

    feat: add options and notifications

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Typography, AppBar } from '@mui/material'
-import VideoPlayer from './components/VideoPlayer'
-import Options from './components/Options'
-import Notifications from './components/Notifications'
-
+import { Typography, AppBar } from '@mui/material';
+import VideoPlayer from './components/VideoPlayer';
+import Options from './components/Options';
+import Notifications from './components/Notifications';
 
 const App = () => {
-
-    return (
-        <div className='wrapper'>
-            <AppBar className='appBar' position='static' color='inherit'>
-                <Typography variant='h2' align='center'>Video Chat App</Typography>
-            </AppBar>
-            <VideoPlayer />
-            <Options>
-                <Notifications />
-            </Options>
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <AppBar className="appBar" position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat App
+        </Typography>
+      </AppBar>
+      <VideoPlayer />
+      <Options>
+        <Notifications />
+      </Options>
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/SocketContext.js
+++ client/src/components/SocketContext.js
@@ -27,7 +27,7 @@ const ContextProvider = ({ children }) => {
 
     socket.on('me', (id) => setMe(id));
 
-    socket.on('calluser', ({ from, name: callerName, signal }) => {
+    socket.on('callUser', ({ from, name: callerName, signal }) => {
       setCall({ isReceivingCall: true, from, name: callerName, signal });
     });
   }, []);
@@ -38,7 +38,7 @@ const ContextProvider = ({ children }) => {
     const peer = new Peer({ initiator: false, trickle: false, stream });
 
     peer.on('signal', (data) => {
-      socket.emit('answercall', { signal: data, to: call.from });
+      socket.emit('answerCall', { signal: data, to: call.from });
     });
 
     peer.on('stream', (currentStream) => {
@@ -54,14 +54,14 @@ const ContextProvider = ({ children }) => {
     const peer = new Peer({ initiator: true, trickle: false, stream });
 
     peer.on('signal', (data) => {
-      socket.emit('calluser', { userToCall: id, signalData: data, from: me, name });
+      socket.emit('callUser', { userToCall: id, signalData: data, from: me, name });
     });
 
     peer.on('stream', (currentStream) => {
       userVideo.current.srcObject = currentStream;
     });
 
-    socket.on('callaccepted', (signal) => {
+    socket.on('callAccepted', (signal) => {
       setCallAccepted(true);
 
       peer.signal(signal);
